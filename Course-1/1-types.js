ed06bdbfd9f74e50b68b83672498f1918290680c
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-02-11 07:55:39 +0200

    data-types

--- Course-1/1-types.js
+++ Course-1/1-types.js
@@ -1,2 +1,11 @@
-var str = 'Hello';
-console.log(str);
+var isFetching = true;
+var isLoading = false;
+var int = 42;
+var float = 4.2;
+var num = 3e10;
+var message = 'Hello Typescript';
+var numberArray = [1, 2, 3, 4, 5, 8, 13];
+var numberArray2 = [1, 2, 3, 4, 5, 8, 13];
+var words = ['Hello', 'Typescript'];
+// Tuple
+var contact = ['Rostyk', 12345678];
--- Course-1/1-types.ts
+++ Course-1/1-types.ts
@@ -2,3 +2,15 @@ const isFetching: boolean = true;
 const isLoading: boolean = false;
 
 const int: number = 42;
+const float: number = 4.2;
+const num: number = 3e10;
+
+const message: string = 'Hello Typescript';
+
+const numberArray: number[] = [1, 2, 3, 4, 5, 8, 13];
+const numberArray2: Array<number> = [1, 2, 3, 4, 5, 8, 13];
+
+const words: string[] = ['Hello', 'Typescript'];
+
+// Tuple
+const contact: [string, number] = ['Rostyk', 12345678];
