01165aba09bef122341e08bd1ded1fc0085cbf7f
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-02-11 11:49:13 +0200

    type

--- Course-1/1-types.ts
+++ Course-1/1-types.ts
@@ -1,37 +1,49 @@
-const isFetching: boolean = true;
-const isLoading: boolean = false;
+// const isFetching: boolean = true;
+// const isLoading: boolean = false;
 
-const int: number = 42;
-const float: number = 4.2;
-const num: number = 3e10;
+// const int: number = 42;
+// const float: number = 4.2;
+// const num: number = 3e10;
 
-const message: string = 'Hello Typescript';
+// const message: string = 'Hello Typescript';
 
-const numberArray: number[] = [1, 2, 3, 4, 5, 8, 13];
-const numberArray2: Array<number> = [1, 2, 3, 4, 5, 8, 13];
+// const numberArray: number[] = [1, 2, 3, 4, 5, 8, 13];
+// const numberArray2: Array<number> = [1, 2, 3, 4, 5, 8, 13];
 
-const words: string[] = ['Hello', 'Typescript'];
+// const words: string[] = ['Hello', 'Typescript'];
 
-// Tuple
-const contact: [string, number] = ['Rostyk', 12345678];
+// // Tuple
+// const contact: [string, number] = ['Rostyk', 12345678];
 
-// Any
-let variable: any = 42;
-// ...
-variable = 'New User';
-variable = [];
+// // Any
+// let variable: any = 42;
+// // ...
+// variable = 'New User';
+// variable = [];
 
-// ============
+// // ============
 
-function sayMyName(name: string): void {
-  console.log(name);
-}
-sayMyName('Zuckerberg');
+// function sayMyName(name: string): void {
+//   console.log(name);
+// }
+// sayMyName('Zuckerberg');
 
-// Never
-function throwError(message: string): never {
-  throw new Error(message);
-}
-function infinite(): never {
-  while (true) {}
-}
+// // Never
+// function throwError(message: string): never {
+//   throw new Error(message);
+// }
+// function infinite(): never {
+//   while (true) {}
+// }
+
+// Type
+type Login = string;
+const login: Login = 'admin';
+// const login2: Login = 2;//error
+
+type ID = string | number;
+const id1: ID = 1234;
+const id2: ID = '1234';
+// const id3: ID = true;//error
+
+type SomeType = string | null | undefined;
