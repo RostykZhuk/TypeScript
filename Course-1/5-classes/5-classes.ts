9b0b63e86fe017b5dc64b2b7360a6b0d4053f322
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-02-14 12:29:21 +0200

    abstract classes

--- Course-1/5-classes/5-classes.ts
+++ Course-1/5-classes/5-classes.ts
@@ -24,3 +24,41 @@ class Car {
   readonly numberOfWheels: number = 4;
   constructor(readonly model: string) {}
 }
+
+// ======================
+class Animal {
+  protected voice: string = '';
+  public color: string = 'black';
+
+  constructor() {
+    this.go();
+  }
+
+  private go() {
+    console.log('go');
+  }
+}
+
+class Cat extends Animal {
+  public setVoice(voice: string): void {
+    this.voice = voice;
+  }
+}
+const cat = new Cat();
+cat.setVoice('test');
+console.log(cat.color);
+
+// ==========================
+abstract class Component {
+  abstract render(): void;
+  abstract info(): string;
+}
+
+class AppComponent extends Component {
+  render(): void {
+    console.log('Component on render');
+  }
+  info(): string {
+    return 'this is info';
+  }
+}
