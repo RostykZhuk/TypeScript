dc05461de8a4dd0af174d70a102cc903a301d599
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-03-06 10:10:56 +0200

    defining and setting the type of todoList using an interface

--- todo-list-react/src/App.tsx
+++ todo-list-react/src/App.tsx
@@ -1,18 +1,43 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useState, ChangeEvent } from 'react';
 import './App.css';
+import { ITask } from './Interfaces';
 
 const App: FC = () => {
   const [task, setTask] = useState<string>('');
   const [deadline, setDeadline] = useState<number>(0);
-  const [todo, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState<ITask[]>([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === 'task') {
+      setTask(event.target.value);
+    } else {
+      setDeadline(Number(event.target.value));
+    }
+  };
+
+  const addTask = (): void => {
+    const newTask = { taskName: task, deadline: deadline };
+    setTodoList([...todoList, newTask]);
+  };
+
   return (
     <div className='App'>
       <div className='header'>
         <div className='inputContainer'>
-          <input type='text' placeholder='Task...' />
-          <input type='number' placeholder='Deadline (in Days)...' />
+          <input
+            type='text'
+            placeholder='Task...'
+            name='task'
+            onChange={handleChange}
+          />
+          <input
+            type='number'
+            placeholder='Deadline (in Days)...'
+            onChange={handleChange}
+            name='deadline'
+          />
         </div>
-        <button>Add Task</button>
+        <button onClick={addTask}>Add Task</button>
       </div>
       <div className='todoList'></div>
     </div>
